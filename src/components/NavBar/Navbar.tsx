3d26d0cdccde08f182d765c99f52f8ce281d2aad
Arlon da Silva Moreira <arlon.moreira@goias.gov.br>
2023-05-21 22:09:41 -0300

    ajuste navbar

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react'
 //styles
 import styles from './Navbar.module.css';
+import logo from '../../logo_min.svg';
 //Hooks
 import { useWindowSize } from 'react-use';
 
@@ -39,7 +40,7 @@ const Navbar = ({sidebarRef}: Props) => {
   };  
 
   return (
-    <div>
+    <div className={styles.container_navbar}>
         <div className={`${styles.menu_open} py-1 px-1 py-sm-1 px-sm-1 py-md-2 px-md-2`}>
             <button className='border-0' type='button' onClick={handleShow}>
                 <div className={`${styles.iconArea} ${styles.active}`} ref={button}>
@@ -49,9 +50,13 @@ const Navbar = ({sidebarRef}: Props) => {
                 </div>
             </button>
         </div>
-        <div>
-            
-        </div>
+        <div className={styles.utils}>
+          <a className={styles.utils_item}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+            </svg>                  
+          </a>
+        </div> 
     </div>
   )
 }
