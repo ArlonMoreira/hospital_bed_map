85f588c67fa54c669bccc8af1f063ab47a6e8709
Arlon Moreira <arlon.moreira@goias.gov.br>
2023-06-15 00:42:39 -0300

    layout page register beds

--- src/pages/RegisterBeds/RegisterBeds.tsx
+++ src/pages/RegisterBeds/RegisterBeds.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 //Styles
 import styles from './RegisterBeds.module.css';
+//Router
+import { Link } from 'react-router-dom';
+//Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
+import { RootState } from '../../store';
+import { hospital } from '../../slices/hospitalSlice';
 //Hooks
 import { useParams } from 'react-router-dom';
 import { useRef } from 'react';
+//Interface
+import { IHospital } from '../../interfaces/Hospital';
 
 type Props = {}
 
@@ -20,6 +29,18 @@ const RegisterBeds = (props: Props) => {
     const handleShow = ():void => {
         page.current?.classList.toggle(styles.open)
     };
+
+    /**
+     * Get data hospital
+     */
+    const dispatch = useDispatch<ThunkDispatch<RootState, any, AnyAction>>();
+    const { hospital: hospitalData }: { hospital: IHospital | null } = useSelector((state: RootState) => state.hospital);
+
+    useEffect(()=>{
+        if(id){
+            dispatch(hospital(id));
+        }
+    }, [dispatch]);
     
     return (
         <div className={`${styles.open} ${styles.area}`} ref={page}>
@@ -27,10 +48,21 @@ const RegisterBeds = (props: Props) => {
             <div className='d-flex'>
                 <div className={`${styles.aside} shadow`}>
                     <div className='p-3'>
+                        <div className={`${styles.navigate} px-0`}>
+                            <Link to='/hospitais'>
+                            <svg width="1.16em" height="1.16em" version="1.0" viewBox="0 0 148 130">
+                                <g transform="translate(0 130) scale(.1 -.1)">
+                                <path d="m338 962c-220-219-288-293-288-312 0-18 67-91 282-307 156-156 291-286 301-289 21-7 57 21 57 43 0 8-106 123-235 256l-235 242 600 5c638 5 620 4 620 50s18 45-620 50l-600 5 235 242c129 133 235 248 235 256 0 17-32 47-51 47-7 0-143-129-301-288z"/>
+                                </g>
+                            </svg>
+                                Hospitais
+                            </Link>
+                        </div>
                         <div className='d-flex justify-content-between align-items-center position-relative'>
-                            <div>
-                                <h5 className='m-0'>XXX</h5>
-                                <p>xxxxxxxx</p>
+                            <div className={`${styles.title}`}>
+                                <span>{hospitalData?.cnes}</span>
+                                <h5 className='m-0'>{hospitalData?.acronym}</h5>
+                                <p>{hospitalData?.name}</p>
                             </div>
                             <div className='position-absolute top-0 end-0'>
                                 <button className='border-0 bg-transparent' onClick={handleShow}>
