21b9d5c4a3b15c19b0dd7e872f6ef583ce2d2d14
Arlon da Silva Moreira <arlon.moreira@goias.gov.br>
2023-08-13 23:19:08 -0300

    style bad bed

--- src/pages/Hospital/ConfigHospital/Beds/Beds.tsx
+++ src/pages/Hospital/ConfigHospital/Beds/Beds.tsx
@@ -63,10 +63,12 @@ const Beds = ({sector}: Props) => {
           beds.map((bed)=>{
             return (
               <div className={`${styles.content} ${styles.item} ${styles[bed.type_occupation_status]}`} key={bed.id}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
-                  <path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/>
-                </svg>    
-                <h5>{bed.name}</h5>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
+                    <path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/>
+                  </svg>    
+                  <h5>{bed.name}</h5>
+                </div>
                 <label className='w-100'>
                   <select
                     className={`${styles.select} form-select`}
@@ -75,11 +77,22 @@ const Beds = ({sector}: Props) => {
                   >
                     {
                       statusData && statusData.map((status,i)=>(
-                          <option key={i} value={status.id?.toString()}>{status.description}</option>
+                          <option key={i} value={status.id?.toString()} className='text-dark'>{status.description}</option>
                       ))
                     }                  
                   </select>
                 </label>
+                {                                   
+                  bed.is_active ? (
+                      <svg viewBox="0 0 576 512" width={'1em'} height={'1em'}>
+                          <path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80v48c0 17.7 14.3 32 32 32s32-14.3 32-32V144C576 64.5 511.5 0 432 0S288 64.5 288 144v48H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H352V144z"/>
+                      </svg>
+                  ) : (
+                      <svg viewBox="0 0 448 512" width={'.86em'} height={'.86em'}>
+                          <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/>
+                      </svg>
+                  )                                 
+                }
               </div>
             )
           })
